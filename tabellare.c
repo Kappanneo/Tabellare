d71b22f000f5832a318c1d6acf4e03e4e0759eb5
Kappanneo <claudio_pannacci@libero.it>
2015-11-17 18:58:26 +0100

    Succesful array sorting.

--- tabellare.c
+++ tabellare.c
@@ -3,19 +3,19 @@
 
 int main(int argc, char *argv[])
 {
-  VET("[numero_variabili] [mintermini_in_decimale]");
+  VET("[numero variabili] [mintermini in decimale]");
 
   int var= unint(argv[1]); // numero variabili
   int mint= argc-2; // numero mintermini
-  char minterm[mint][var+2]; // array bidimensionale: ogni riga è un minterm
-  // [numero di 1 nelle variabili] [variabili] ['\0']
+  char minterm[mint][var+2]; // array bidimensionale
+  // ogni riga: [numero di 1 nel minterm] [minterm] ['\0']
 
   ABC(var);
 
   for(int x=0; x < mint; x++) // per ogni minterm
     {
       int num= unint(argv[x+2]); // valore decimale
-	  
+
       for(int v= var; v > 0; v--) // converte in binario
 	{
 	  minterm[x][v]= (char) nascii(num%2);
@@ -25,9 +25,14 @@ int main(int argc, char *argv[])
       minterm[x][var+1]= '\0';
     }
 
+  for(int z= mint; z > 0; z--) // ordina i minterm
+    for(int x= 1; x < z; x++)
+      if(minterm[x-1][0] > minterm [x][0]) // in base al numero di 1
+	scambia(minterm[x-1],minterm[x]);
+
   for(int x=0; x < mint; x++) // per ogni minterm
     {
-      printf("%s", &minterm[x][1]);
+      printf("%s", &minterm[x][1]); //stampa il minterm
       printf(" %c\n", minterm[x][0]); // stampa il numero di '1' 
     }
   puts("-----------------");
